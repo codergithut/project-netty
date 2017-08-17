b79e9cbf5aac48ff99be1bb91192c00b332c38b3
tianjian <1731857742@qq.com>
2017-08-17 20:05:50 +0800

    U盘date e

---
+++ src/main/java/io/websocket/handler/SecureChatServerInitializer.java
@@ -0,0 +1,30 @@
+package io.websocket.handler;
+
+import io.netty.channel.Channel;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslHandler;
+import io.websocket.pipeline.ChatServerInitializer;
+
+import javax.net.ssl.SSLEngine;
+
+/**
+ * @author <a href="mailto:Administrator@gtmap.cn">Administrator</a>
+ * @version 1.0, 2017/8/17
+ * @description
+ */
+public class SecureChatServerInitializer extends ChatServerInitializer {
+    private final SslContext context;
+
+    public SecureChatServerInitializer(ChannelGroup group, SslContext context) {
+        super(group);
+        this.context = context;
+    }
+
+    protected void initChannel(Channel ch) throws Exception {
+        super.initChannel(ch);
+        SSLEngine engine = context.newEngine(ch.alloc());
+        engine.setUseClientMode(false);
+        ch.pipeline().addFirst(new SslHandler(engine));
+    }
+}
